2262de3c6c11ec55747474adc8c9f32af0c870b8
Altraya <karakayn@darluok-server.com>
2015-10-27 15:04:09 +0100

    -> Recommit cause of corrupted files

--- src/luciolecompta/Models/FactureManager.java
+++ src/luciolecompta/Models/FactureManager.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -17,6 +18,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import luciolecompta.DateUtil;
 
 /**
  * Manager des factures
@@ -47,24 +49,53 @@ public class FactureManager {
         Statement stmt = null;
         ObservableList<Facture> bill = FXCollections.observableArrayList();
         
-        try {
-            stmt = db.createStatement();
-            
-            System.out.println("Selection des factures du client n°"+client.getId()+" en base de données.");
-            ResultSet rs = stmt.executeQuery("SELECT id, sujet, dateDevis, type, prixTotal, dateExecution, datePaiement FROM FACTURE");
-            while (rs.next()) {
-                Date tmpDateDevis = new Date(rs.getString("dateDevis"));
-                System.out.println("String : "+rs.getString("dateDevis"));
-                System.out.println("Rs : "+rs.getDate("dateDevis")+"\n");
-                /*Facture fac = new Facture(rs.getInt("id"), rs.getString("sujet"), rs.getDate("dateDevis"), rs.getString("type"), rs.getInt("total"), rs.getDate("dateExecution"), rs.getDate("datePaiement"));
-                //System.out.println("Dans getClientdata de clientManager | id =" + rs.getInt("id")+ " nom = " + rs.getString("nom") + " Entreprise " +rs.getString("entreprise")+ " Adresse 1 " + rs.getString("adresse1") + "\n");
-                bill.add(fac);*/
+        if(client.getId() == 0)
+            System.out.println("Erreur : id du client n'est pas défini");
+        else{
+            try {
+                stmt = db.createStatement();
+
+                System.out.println("Selection des factures du client n°"+client.getId()+" en base de données.");
+                ResultSet rs = stmt.executeQuery("SELECT CLIENT.id, sujet, dateDevis, type, prixTotal, dateExecution, datePaiement FROM FACTURE JOIN CLIENT ON (CLIENT.id = FACTURE.idClient) WHERE CLIENT.id = "+client.getId());
+                System.out.println(rs.next());
+                
+                while (rs.next()) {
+                    //Need to do this weird think because i cant use rs.getDate() cause it return a timestamp and i can't change this or i always have error / see branch test2
+                    //Transformation to date
+                    Date dateDevis = DateUtil.getDate(rs.getObject("dateDevis"));
+                    Date dateExecution = DateUtil.getDate(rs.getObject("dateExecution"));
+                    Date datePaiement = DateUtil.getDate(rs.getObject("datePaiement"));
+                    //Check if something is null
+                    int prixTotal = 0;
+                    if(rs.getString("prixTotal") != null)
+                        prixTotal = rs.getInt("prixTotal");
+                    String type = "";
+                    if(rs.getString("type") != "" || rs.getString("type") != null)
+                        type = rs.getString("type");
+                    System.out.println("Rs : id : "+rs.getObject("id")+ "\n");
+                    System.out.println("sujet : "+rs.getObject("sujet"));
+                    System.out.println("dateDevis :"+rs.getObject("dateDevis"));
+                    System.out.println("type :"+rs.getString("type"));
+                    System.out.println("prixTotal :"+prixTotal);
+                    System.out.println("dateExecution : "+rs.getObject("dateExecution"));
+                    System.out.println("datePaiement : "+rs.getObject("datePaiement"));
+
+                    Facture fac = new Facture(rs.getInt("id"), rs.getString("sujet"), dateDevis, type, prixTotal, dateExecution, datePaiement);
+                    //System.out.println("Dans getClientdata de clientManager | id =" + rs.getInt("id")+ " nom = " + rs.getString("nom") + " Entreprise " +rs.getString("entreprise")+ " Adresse 1 " + rs.getString("adresse1") + "\n");
+                    System.out.println("Objet facture crée");
+                    bill.add(fac);
+                    System.out.println("Objet facture ajouté à la liste");
+                }
+                System.out.println("Ok.");
+                stmt.close();
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.out.println("Exception dans facture manager");
+                System.exit(0);
             }
-            System.out.println("Ok.");
-            stmt.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
+        }
+        for(int i = 0; i<bill.size(); i++){
+            System.out.println(bill.get(i).toString());
         }
         return bill;
     }
